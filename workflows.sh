e4562d2adf1972872ae6f2b287b2222c838c763c
Krzysztof Sakrejda <krzysztof.sakrejda@gmail.com>
2015-12-05 11:35:00 -0500

    Initial commit.

---
+++ workflows.sh
@@ -0,0 +1,20 @@
+## The following relative paths are relative to the 'servestan'
+## directory.
+
+# Cloning grpc, then 'make'
+git clone https://github.com/grpc/grpc.git
+cd grpc
+git submodule update --init
+make  ## the grpc plugin ends up in bins/opt
+
+# Use the protobuf compiler to produce the service interface
+# code, requires a 'grpc' directory clone from github parallel
+# to the servestan directory.
+../grpc/bins/opt/protobuf/protoc \
+  --grpc_out=src  \
+  --plugin=protoc-gen-grpc=../grpc/bins/opt/grpc_cpp_plugin \
+  ./proto/stanc.proto
+
+# Use the protobuf compiler to produce message interface
+# code:
+../grpc/bins/opt/protobuf/protoc --cpp_out=src ./proto/stanc.proto
