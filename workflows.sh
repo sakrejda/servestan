9c5a25047c7aff0fec825c9effc8dc267392b135
Krzysztof Sakrejda <krzysztof@fawkes.io>
2015-12-17 18:26:30 -0500

    Split compile/link but still with that one linking error, check what cmdstan does.

--- workflows.sh
+++ workflows.sh
@@ -33,38 +33,39 @@ lib/grpc/bins/opt/protobuf/protoc \
 lib/grpc/bins/opt/protobuf/protoc --cpp_out=src ./proto/stanc.proto
 
 # Oy, where are all the pieces hiding:
-g++ -std=c++11 -O3 -v \
-  -I src/ \
+g++ -std=c++11 -O3 -v -Wall -pipe -c \
+  -DBOOST_RESULT_OF_USE_TR1 -DBOOST_NO_DECLTYPE -DBOOST_DISABLE_ASSERTS \
+  -I src \
+  -I lib/stan/src/ \
+  -isystem lib/stan/lib/stan_math_2.9.0/ \
+  -isystem lib/stan/lib/stan_math_2.9.0/lib/eigen_3.2.4 \
+  -isystem lib/stan/lib/stan_math_2.9.0/lib/boost_1.58.0 \
   -I lib/grpc/include/ \
   -I lib/grpc/third_party/protobuf/src/ \
-  -I lib/stan/src/ \
-  -I lib/stan/lib/stan_math_2.9.0/lib/boost_1.58.0/ \
-  -o servestan \
-  src/servestan/servestan.cpp \
-  -L lib/grpc/libs/opt/ \
-  -L lib/stan/bin/stan \
-  -L lib/stan/bin/ \
-  -lgrpc++_unsecure -lgrpc -lgpr -lprotobuf -lpthread -ldl \
-  -lstanc 
-
-
-g++ -std=c++11 -O3 -v \
-  -I src/ \
-  -I lib/grpc/include/ \
-  -I lib/grpc/third_party/protobuf/src/ \
-  -I lib/stan/src/ \
-  -I lib/stan/lib/stan_math_2.9.0/lib/boost_1.58.0/ \
-  -o servestan \
   src/servestan/servestan.cpp \
   src/proto/stanc.grpc.pb.cc \
-  src/proto/stanc.pb.cc \
+  src/proto/stanc.pb.cc 
+
+g++ -std=c++11 -O3 -v -Wall -pipe \
+  -DBOOST_RESULT_OF_USE_TR1 -DBOOST_NO_DECLTYPE -DBOOST_DISABLE_ASSERTS \
+  -isystem lib/stan/src/ \
+  -isystem lib/stan/lib/stan_math_2.9.0/ \
+  -isystem lib/stan/lib/stan_math_2.9.0/lib/boost_1.58.0 \
+  -isystem lib/stan/lib/stan_math_2.9.0/lib/eigen_3.2.4 \
+  -o bin/servestan.o \
+  -isystem lib/stan/lib/stan_math_2.9.0/ \
+  servestan.o \
+  stanc.grpc.pb.o \
+  stanc.pb.o \
+  lib/stan/bin/libstanc.a \
   lib/grpc/libs/opt/libgrpc++_unsecure.so \
   lib/grpc/libs/opt/libgrpc_unsecure.so \
   lib/grpc/libs/opt/libgrpc.so \
   lib/grpc/libs/opt/libgpr.so \
-  lib/stan/bin/libstanc.a \
   lib/grpc/third_party/protobuf/src/.libs/libprotobuf.a \
-  -lpthread -ldl -lboost
+  -lpthread -ldl 
+
+
 
 
 
